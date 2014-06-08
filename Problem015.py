84dfc7e873636cc07c86ad639b19050ae6dd1138
Matias Christensen <mattivc@gmail.com>
2014-06-08 14:49:42 +0200

    Improved solution to Problem 15 by using combinatoric solution.

--- Problem015.py
+++ Problem015.py
@@ -1,16 +1,7 @@
-dataCache = { (0,0): 1 }
+from math import factorial
 
+# paths = (x + Y)! / (x! * y!)
 def get_path_number(x, y):
-	val = dataCache.get((x,y), -1) # try to get data from cache
-
-	if (val == -1): # calculate the data if not found in cache
-		if (x < 0):
-			val = 0
-		elif (y < 0):
-			val = 0
-		else:
-			val = get_path_number(x-1,y) + get_path_number(x, y-1)
-		dataCache.update({(x,y):val}) # add the data to cache
-	return val
+	return factorial(x + y) / (factorial(x) * factorial(y))
 
 print(get_path_number(20,20))
