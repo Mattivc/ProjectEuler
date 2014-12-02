31a74dc2ee44a8d8983201f3c49698a4dee6fb28
Matias Christensen <mattivc@gmail.com>
2014-12-02 15:28:17 +0100

    Identical solution to 18 and 67

---
+++ Problem018.py
@@ -0,0 +1,25 @@
+
+triangle = []
+
+with open('Problem018_data.txt') as f:
+	for line in f:
+		line_array = []
+		for n in line.split(' '):
+			line_array.append(int(n))
+		triangle.append(line_array)
+
+rows = len(triangle)
+memo = {}
+
+def max_path_sum(row, element):
+	if row >= rows: # we have moved past the last row
+		return 0
+
+	if (row, element) in memo:
+		return memo[(row, element)]
+
+	value = max(max_path_sum(row+1, element), max_path_sum(row+1, element+1)) + triangle[row][element]
+	memo[(row, element)] = value
+	return value
+
+print max_path_sum(0,0)
